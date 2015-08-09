2f9a98e5ead1a822d6e60f1ad28990a379880305
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-08-09 13:49:16 -0700

    checkin

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿
+var edge = require('edge')
+  , path = require('path')
+  , dapperService = edge.func(path.join('./lib/DapperService.cs'))
+
+var helloworld = edge.func(function() {/*
+    async (input) => {
+      return ".NET Welcomes " + input.ToString();
+    }
+    
+  */})
+
+helloworld('JavaScript', function(err, result) {
+  if (err) throw err;
+  console.log(result);
+})
